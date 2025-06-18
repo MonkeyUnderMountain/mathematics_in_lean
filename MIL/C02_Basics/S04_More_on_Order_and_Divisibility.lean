6b76f71e8fa8a59609ab41556cc7ee69c62dab8d
tianle_laptop <loveandjustice@88.com>
2025-06-18 15:53:26 +0800

    finish C02 S04

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -38,18 +38,94 @@ example : min a b = min b a := by
     apply min_le_right
     apply min_le_left
 
+#check (le_of_max_le_left : max a b ≤ c → a ≤ c)
+#check (max_le_max : a ≤ b → c ≤ d → max a c ≤ max b d)
+#check (le_max_left a b : a ≤ max a b)
+#check (le_trans : a ≤ b → b ≤ c → a ≤ c)
+
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  · show max a b ≤ max b a
+    apply max_le
+    · show a ≤ max b a
+      apply le_max_right
+    · show b ≤ max b a
+      apply le_max_left
+  · show max b a ≤ max a b
+    apply max_le
+    · show b ≤ max a b
+      apply le_max_right
+    · show a ≤ max a b
+      apply le_max_left
+
+theorem double_min_le_the_first : min (min a b) c ≤ a := by
+  have h₁ : min a b ≤ a := min_le_left a b
+  have h₂ : min (min a b) c ≤ min a b := min_le_left (min a b) c
+  apply le_trans h₂ h₁
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+  · show min (min a b) c ≤ min a (min b c)
+    apply le_min
+    · show min (min a b) c ≤ a
+      apply double_min_le_the_first
+    · show min (min a b) c ≤ min b c
+      apply le_min
+      · show min (min a b) c ≤ b
+        rw[min_comm a b]
+        apply double_min_le_the_first
+      · show min (min a b) c ≤ c
+        apply min_le_right
+  · show min a (min b c) ≤ min (min a b) c
+    apply le_min
+    · show min a (min b c) ≤ min a b
+      apply le_min
+      · show min a (min b c) ≤ a
+        apply min_le_left
+      · show min a (min b c) ≤ b
+        rw [min_comm]
+        apply double_min_le_the_first
+    · show min a (min b c) ≤ c
+      rw[min_comm, min_comm b c]
+      apply double_min_le_the_first
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  · show min a b + c ≤ a + c
+    have h₁ : min a b ≤ a := min_le_left a b
+    apply add_le_add_right h₁ c
+  · show min a b + c ≤ b + c
+    have h₂ : min a b ≤ b := min_le_right a b
+    apply add_le_add_right h₂ c
+
+
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  · show min a b + c ≤ min (a + c) (b + c)
+    apply aux
+  · show min (a + c) (b + c) ≤ min a b + c
+    have h₁ : min (a+c) (b+c) - c ≤ min a b := by
+      apply le_min
+      · show min (a+c) (b+c) - c ≤ a
+        have h₃ : min (a+c) (b+c) ≤ a + c := min_le_left (a+c) (b+c)
+        linarith
+      · show min (a+c) (b+c) - c ≤ b
+        have h₄ : min (a+c) (b+c) ≤ b + c := min_le_right (a+c) (b+c)
+        linarith
+    linarith
+
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  have h : |a| ≤ |a - b| + |b| := by
+    have h' : |a - b + b| ≤ |a - b| + |b| := abs_add (a-b) b
+    have h'' : a - b + b = a := by linarith
+    rw [h''] at h'
+    exact h'
+  linarith
+
 end
 
 section
@@ -65,8 +141,19 @@ example : x ∣ y * x * z := by
 example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
+#check (dvd_mul_right)
+
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  have h₁ : x ∣ y * (x * z) := by
+    have h' : y * (x * z) = x * (y * z) := by linarith
+    rw [h']
+    apply dvd_mul_right
+  have h₂ : x ∣ x ^ 2 := dvd_mul_left _ _
+  have h₃ : x ∣ w ^ 2 := by
+    have h'' : w ∣ w ^ 2 := dvd_mul_left _ _
+    apply dvd_trans h h''
+  have h₄ : x ∣ y * (x * z) + x ^ 2 := dvd_add h₁ h₂
+  apply dvd_add h₄ h₃
 end
 
 section
@@ -78,7 +165,17 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply dvd_antisymm
+  · show Nat.gcd m n ∣ Nat.gcd n m
+    apply dvd_gcd
+    · show Nat.gcd m n ∣ n
+      apply Nat.gcd_dvd_right
+    · show Nat.gcd m n ∣ m
+      apply Nat.gcd_dvd_left
+  · show Nat.gcd n m ∣ Nat.gcd m n
+    apply dvd_gcd
+    · show Nat.gcd n m ∣ m
+      apply Nat.gcd_dvd_right
+    · show Nat.gcd n m ∣ n
+      apply Nat.gcd_dvd_left
 end
-
-
